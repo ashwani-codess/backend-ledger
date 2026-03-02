2986873f5baf03ac78ede5519e700aad27f93eb5
ashwani-codess <saraswatashwani22@gmail.com>
2026-03-02 18:32:38 +0530

    Added animated server status page

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const cookieParser = require("cookie-parser");
 
+
 const authRouter =require('../src/routes/auth.routes');
 const accountRouter= require("./routes/account.routes");
 const transactionRouter= require('./routes/transaction.routes')
 
 const app = express();
+
+app.use(express.static("public"));
 
 app.use(express.json());   // body read karega
 
@@ -16,5 +19,12 @@ app.use("/api/auth", authRouter);
 app.use("/api/accounts", accountRouter);
 
 app.use("/api/transaction", transactionRouter);
+
+app.get("/health", (req, res) => {
+  res.json({
+    status: "OK",
+    database: "✅ Connected"
+  });
+});
 
 module.exports = app;
